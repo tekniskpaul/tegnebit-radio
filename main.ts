5f94455392bc7c38e39ef4fa9527be8e815114e6
Paul Mikaelsen <125556258+tekniskpaul@users.noreply.github.com>
2024-02-02 14:24:01 +0100

    Took away "toggle" which was unnessesary

--- main.ts
+++ main.ts
@@ -1,25 +1,24 @@
 let emoji = ""
-input.onButtonPressed(Button.AB, function () {
-    let Y = 0
-    let X = 0
-    emoji = ""
-    led.toggle(X, Y)
-    for (let Xindex = 0; Xindex <= 4; Xindex++) {
-        for (let Yindex = 0; Yindex <= 4; Yindex++) {
-            if (led.point(Xindex, Yindex)) {
-                emoji = "" + emoji + "1"
-            } else {
-                emoji = "" + emoji + "0"
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    let ferdig = 0
+    if (ferdig == 1) {
+        emoji = ""
+        for (let Xindex = 0; Xindex <= 4; Xindex++) {
+            for (let Yindex = 0; Yindex <= 4; Yindex++) {
+                if (led.point(Xindex, Yindex)) {
+                    emoji = "" + emoji + "1"
+                } else {
+                    emoji = "" + emoji + "0"
+                }
+                led.toggle(Xindex, Yindex)
+                basic.pause(30)
+                led.toggle(Xindex, Yindex)
             }
-            led.toggle(Xindex, Yindex)
-            basic.pause(30)
-            led.toggle(Xindex, Yindex)
         }
+        radio.sendString(emoji.substr(0, 15))
+        basic.pause(10)
+        radio.sendString(emoji.substr(15, 10))
+        basic.pause(10)
+        radio.sendString("ferdig")
     }
-    led.toggle(X, Y)
-    radio.sendString(emoji.substr(0, 15))
-    basic.pause(10)
-    radio.sendString(emoji.substr(15, 10))
-    basic.pause(10)
-    radio.sendString("ferdig")
 })
